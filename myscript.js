d9548b64e2a7c6a88772fcce688cb2048899c958
ffpcomms <47221200+ffpcomms@users.noreply.github.com>
2019-12-04 14:24:23 -0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,55 @@
+var map = L.map('map').setView([0, 25], 3);
+
+
+
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+		maxZoom: 4,
+		attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
+		}).addTo(map);
+
+
+	
+// get color depending on Office No value
+	function getColor(d) {
+			if(d === 'Both'){
+				return '#BDA8BA';
+			}else if(d === 'FFP'){
+				return '#f88379';
+			}else{
+				return '#82B2DC';
+			}
+	}
+	
+	function style(feature) {
+		return {
+			fillColor: getColor(feature.properties.Office),
+			weight: 2,
+			opacity: 1,
+			color: '#8C8985',
+			dashArray: '3',
+			fillOpacity: 0.7
+		};
+	}
+	
+	
+L.geoJson(bhaCountries, {
+	style: style,
+	onEachFeature: function (feature, layer) {
+		layer.bindTooltip(feature.properties.SOVEREIGNT);
+	}
+}).addTo(map);
+
+
+var legend = L.control({position: 'topright'});
+
+legend.onAdd = function(map) {
+  var div = L.DomUtil.create("div", "legend");
+  //div.innerHTML += "<h4>Managed By:</h4>";
+  div.innerHTML += '<i style="background: #f88379"></i><span>FFP</span><br>';
+  div.innerHTML += '<i style="background: #82B2DC"></i><span>OFDA</span><br>';
+  div.innerHTML += '<i style="background: #BDA8BA"></i><span>Both FFP and OFDA</span><br>';
+  return div;
+};
+
+legend.addTo(map); 
+
